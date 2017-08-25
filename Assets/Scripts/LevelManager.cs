464136dd0eb5e00b69b1447d67a86397598d9945
Bharat <bharat.monavarthi@hotmail.com>
2017-08-25 22:08:49 +0530

    Added spash screen, menu, music and navigation

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float loadTime;
+	
+	void Start () {
+		if(Application.loadedLevel == 0) {
+			Invoke("LoadNextLevel", loadTime);
+		}
+	}
+
+	public void LoadLevel (string name) {
+		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest () {
+		Debug.Log ("Quit game requested");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}	
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusic;
+	private AudioSource audioSource;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		if(levelMusic[level]) {
+			audioSource.clip = levelMusic[level];
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
