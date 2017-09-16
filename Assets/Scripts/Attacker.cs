45d41b43982b0639072507fa4a2258d9909cc827
Bharat <bharat.monavarthi@hotmail.com>
2017-09-16 19:13:10 +0530

    Created Attcker and Defender scripts

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)] public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D thisRigidBody = gameObject.AddComponent<Rigidbody2D>();
+		thisRigidBody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter2D () {
+		Debug.Log(name + "enter trigger");
+	}
+}
