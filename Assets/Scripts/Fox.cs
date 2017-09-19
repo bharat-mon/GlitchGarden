8acf75a5d9e7d2e271b755ab30e89b4f6c22ed56
Bharat <bharat.monavarthi@hotmail.com>
2017-09-19 23:26:53 +0530

    Added seperate scripts for attackers for thier unique behaviours

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Attacker))]
+public class Fox : MonoBehaviour {
+
+	private Animator animator;
+	private Attacker attacker;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		attacker = GetComponent<Attacker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		GameObject defender = collider.gameObject;
+		
+		if (!defender.GetComponent<Defender>()) {
+			return;
+		}
+		
+		if (defender.GetComponent<Stone>()) {
+			animator.SetTrigger("jumpTrigger");
+		} else {
+			animator.SetBool("isAttacking", true);
+			attacker.Attack(defender);
+		}
+	}
+}
---
+++ Assets/Scripts/Lizard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Attacker))]
+public class Lizard : MonoBehaviour {
+
+	private Animator animator;
+	private Attacker attacker;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		attacker = GetComponent<Attacker>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D (Collider2D collider) {
+		GameObject defender = collider.gameObject;
+		
+		if (defender.GetComponent<Defender>()) {
+			animator.SetBool("isAttacking", true);
+			attacker.Attack(defender);
+		} else {
+			return;
+		}
+	}
+}
