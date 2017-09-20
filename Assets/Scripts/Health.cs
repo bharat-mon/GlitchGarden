9a20f999488c7fb66f511d69b644b8b4b2191a0a
Bharat <bharat.monavarthi@hotmail.com>
2017-09-21 00:12:21 +0530

    Created a health tracking script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public float health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void DamageDealt (float damage) {
+		health -= damage;
+		if (health <= 0) {
+			DestroyObject();
+		}	
+	}
+	
+	// If we add a death animation
+	void DestroyObject () {
+		Destroy(gameObject);
+	}
+}
