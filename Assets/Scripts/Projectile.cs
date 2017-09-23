e1c40f10d6916a686af79fbb894347b235948597
Bharat <bharat.monavarthi@hotmail.com>
2017-09-23 20:25:41 +0530

    Added code for the trigger to deal damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
 
 	public float speed, damage;
+	private GameObject attacker;
+	private Health health;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +15,14 @@ public class Projectile : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter2D (Collider2D collider) {
+		attacker = collider.gameObject;
+		if (attacker.GetComponent<Attacker>()) {
+			health = attacker.GetComponent<Health>();
+			health.DamageDealt(damage);
+			Destroy(gameObject);
+		}
 	}
 }
