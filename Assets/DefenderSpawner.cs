5e855ff7e125a2e4de914e43120cab24a20f8be9
Bharat <bharat.monavarthi@hotmail.com>
2017-09-24 20:47:59 +0530

    Created the script to spawn defenders by selecting them from the buttons bar

---
+++ Assets/DefenderSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefenderSpawner : MonoBehaviour {
+
+	public Camera myCamera;
+	
+	private GameObject defenderParent;
+
+	// Use this for initialization
+	void Start () {
+		defenderParent = GameObject.Find("Defenders");
+		if (!defenderParent) {
+			defenderParent = new GameObject("Defenders");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown () {
+		Vector2 rawPosition = WorldPointTranslator();
+		Vector2 spawnPosition = SnapToGrid(rawPosition);
+		GameObject newDefender = Instantiate(Button.selectedDefender, spawnPosition, Quaternion.identity) as GameObject;
+		newDefender.transform.parent = defenderParent.transform;
+	}
+	
+	Vector2 WorldPointTranslator () {
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+		float cameraDistance = 10f;
+		
+		Vector2 worldPoint = myCamera.ScreenToWorldPoint(new Vector3 (mouseX, mouseY, cameraDistance));
+		return worldPoint;
+	}
+	
+	Vector2 SnapToGrid (Vector2 rawWorldPoint) {
+		float newX = Mathf.RoundToInt(rawWorldPoint.x);
+		float newY = Mathf.RoundToInt(rawWorldPoint.y);
+		Vector2 snapPosition = new Vector2 (newX, newY);
+		return snapPosition;
+	}
+}
