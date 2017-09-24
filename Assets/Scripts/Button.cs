0da1aef07ddad99eac745bae268d1f8fba5fb764
Bharat <bharat.monavarthi@hotmail.com>
2017-09-24 19:32:36 +0530

    Created a script for button functionality

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public GameObject defenderPrefab;
+	public static GameObject selectedDefender;
+
+	private Color inactiveColor = Color.black;
+	private Color activeColor = Color.white;
+	private SpriteRenderer spriteRenderer;
+	private Button[] buttonArray;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		spriteRenderer.color = inactiveColor;
+		buttonArray = GameObject.FindObjectsOfType<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown () {
+		foreach (Button thisButton in buttonArray) {
+			thisButton.spriteRenderer.color = inactiveColor;
+		}
+		spriteRenderer.color = activeColor;
+		selectedDefender = defenderPrefab;
+	}
+}
