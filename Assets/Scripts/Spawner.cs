fed08ea857a70ebd6966440a6ea422adfc3c2246
Bharat <bharat.monavarthi@hotmail.com>
2017-09-25 23:33:50 +0530

    Script to spawn attackers at defined time

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] attackerArray;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject thisAttacker in attackerArray) {
+			if (isTimeToSpawn(thisAttacker)) {
+				Spawn(thisAttacker);
+			}
+		}
+	}
+	
+	void Spawn (GameObject myGameObject) {
+		GameObject thisAttacker = Instantiate(myGameObject) as GameObject;
+		thisAttacker.transform.parent = this.transform;
+		thisAttacker.transform.position = this.transform.position;
+	}
+	
+	bool isTimeToSpawn (GameObject attacker) {
+		Attacker thisAttacker = attacker.GetComponent<Attacker>();
+		
+		float meanSpawnDelay = thisAttacker.spawnRate;
+		float spawnsPerSecond = 1 / meanSpawnDelay;
+		float spawnTreshold = spawnsPerSecond * Time.deltaTime / 5;
+		
+		if (Time.deltaTime > meanSpawnDelay) {
+			Debug.LogWarning("Spawn rate capped by frame rate");
+		} else if (Random.value < spawnTreshold) {
+			return true;
+		} else {
+			return false;
+		}
+		
+		return true;
+	}
+}
