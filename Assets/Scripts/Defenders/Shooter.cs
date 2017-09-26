bfb7073688a2e425cd36a964642764dea3487050
Bharat <bharat.monavarthi@hotmail.com>
2017-09-26 23:03:36 +0530

    Added methods to detect enemy spawn in the defenders lane and set them into attacking if any

--- Assets/Scripts/Defenders/Shooter.cs
+++ Assets/Scripts/Defenders/Shooter.cs
@@ -6,6 +6,8 @@ public class Shooter : MonoBehaviour {
 	public GameObject projectile, projectileLauncher;
 	
 	private GameObject projectileParent;
+	private Animator animator;
+	private Spawner mySpawner;
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +15,44 @@ public class Shooter : MonoBehaviour {
 		if (!projectileParent) {
 			projectileParent = new GameObject("Projectiles");
 		}
+		
+		animator = GameObject.FindObjectOfType<Animator>();
+		
+		SetMyLaneSpawner();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (IsAttackerAheadInLane()) {
+			animator.SetBool("isAttacking", true);
+		} else {
+			animator.SetBool("isAttacking", false);
+		}
+	}
 	
+	void SetMyLaneSpawner () {
+		Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+		foreach (Spawner spawner in spawnerArray) {
+			if (spawner.transform.position.y == this.transform.position.y) {
+				mySpawner = spawner;
+				return;
+			}
+		}
+		Debug.LogError(name + " can't find spawner in lane");
+	}
+	
+	bool IsAttackerAheadInLane () {
+		if (mySpawner.transform.childCount <= 0) {
+			return false;
+		}
+		
+		foreach (Transform attacker in mySpawner.transform) {
+			if (attacker.transform.position.x >= this.transform.position.x) {
+				return true;
+			}
+		} 
+		
+		return false;
 	}
 	
 	private void Fire () {
