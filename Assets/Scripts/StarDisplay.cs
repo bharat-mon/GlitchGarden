99da8c733850277086633809eae553be435dba71
Bharat <bharat.monavarthi@hotmail.com>
2017-09-27 22:28:18 +0530

    Added methods to keep count of the stars and add stars when needed and subtract stars when used

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+	private Text myText;
+	private int starCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text>();
+		myText.text = starCount.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void AddStars (int amount) {
+		starCount += amount;
+		UpdateDisplay();
+	}
+	
+	public void UseStars (int amount) {
+		starCount -= amount;
+		UpdateDisplay();
+	}
+	
+	private void UpdateDisplay () {
+		myText.text = starCount.ToString();
+	}
+}
