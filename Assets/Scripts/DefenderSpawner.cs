ddc08ecfacba046d8f3992884819bb0edede0b7d
Bharat <bharat.monavarthi@hotmail.com>
2017-09-28 23:42:48 +0530

    Added condition to be able to spawn defenders only if the player has enough stars

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -6,9 +6,11 @@ public class DefenderSpawner : MonoBehaviour {
 	public Camera myCamera;
 	
 	private GameObject defenderParent;
+	private StarDisplay starDisplay;
 
 	// Use this for initialization
 	void Start () {
+		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
 		defenderParent = GameObject.Find("Defenders");
 		if (!defenderParent) {
 			defenderParent = new GameObject("Defenders");
@@ -23,7 +25,17 @@ public class DefenderSpawner : MonoBehaviour {
 	void OnMouseDown () {
 		Vector2 rawPosition = WorldPointTranslator();
 		Vector2 spawnPosition = SnapToGrid(rawPosition);
-		GameObject newDefender = Instantiate(Button.selectedDefender, spawnPosition, Quaternion.identity) as GameObject;
+		GameObject defender = Button.selectedDefender;
+		int starCost = defender.GetComponent<Defender>().starCost;
+		if (starDisplay.UseStars(starCost) == StarDisplay.Status.SUCCESS) {
+			SpawnDefender (spawnPosition, defender);
+		} else {
+			Debug.Log ("Insufficient stars");
+		}
+	}
+
+	void SpawnDefender (Vector2 spawnPosition, GameObject defender) {
+		GameObject newDefender = Instantiate (defender, spawnPosition, Quaternion.identity) as GameObject;
 		newDefender.transform.parent = defenderParent.transform;
 	}
 	
