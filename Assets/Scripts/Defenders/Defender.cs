2eb20cfa5b7fd555bf9d4569a28a0063c2c2f806
Bharat <bharat.monavarthi@hotmail.com>
2017-09-28 23:41:06 +0530

    Added a star cost to use a defender

--- Assets/Scripts/Defenders/Defender.cs
+++ Assets/Scripts/Defenders/Defender.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Defender : MonoBehaviour {
 
 	private StarDisplay starDisplay;
+	public int starCost = 10;
 
 	// Use this for initialization
 	void Start () {
