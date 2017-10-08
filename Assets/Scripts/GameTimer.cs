6199b6be4657d5c8e576057fc4c126cf9669a39b
Bharat <bharat.monavarthi@hotmail.com>
2017-10-08 16:37:33 +0530

    Script to control the game timer slider for time left to defend

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelTime = 100;
+	
+	private LevelManager levelManager;
+	private AudioSource audioSource;
+	private Slider slider;
+	private bool islevelComplete = true;
+
+	
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		audioSource = GetComponent<AudioSource>();
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Time.timeSinceLevelLoad / levelTime;
+		if (Time.timeSinceLevelLoad >= levelTime && islevelComplete) {
+			audioSource.Play();
+			Invoke ("LoadNextLevel", audioSource.clip.length);
+			islevelComplete = false;
+		}
+	}
+	
+	void LoadNextLevel () {
+		levelManager.LoadLevel("Win");
+	}
+}
